b7c8bd1490223c5125436990bceff6b38275054f
Parham Alvani <parham.alvani@gmail.com>
2015-11-17 19:35:34 +0330

    Host specification was added into nom.

---
+++ nom/host.go
@@ -0,0 +1,85 @@
+/*
+ * In The Name Of God
+ * ========================================
+ * [] File Name : host.go
+ *
+ * [] Creation Date : 16-11-2015
+ *
+ * [] Created By : Elahe Jalalpour (el.jalalpour@gmail.com)
+ * =======================================
+ */
+/*
+ * Copyright (c) 2015 Elahe Jalalpour.
+ */
+
+package nom
+
+import (
+	"encoding/gob"
+	"encoding/json"
+	"fmt"
+)
+
+// TODO(elahe): add documentation ..
+type HostLeft Host
+type HostJoined Host
+
+/*
+ * Host represnts a end point element, such as pc :)
+ */
+type Host struct {
+	ID      HostID
+	Net     UID
+	MACAddr MACAddr
+}
+
+/*
+ * HostID is the ID of a host. This must be unique among all hosts in the
+ * network
+ */
+type HostID string
+
+func (h Host) String() string {
+	return fmt.Sprintf("Host %s (mac=%v)", string(h.ID), h.MACAddr)
+}
+
+/*
+ * UID converts id into a UID.
+ */
+func (id HostID) UID() UID {
+	return UID(id)
+}
+
+/*
+ * UID returns the node's unique ID. This id is in the form of net_id$$host_id.
+ */
+func (n Host) UID() UID {
+	return UID(string(n.ID))
+}
+
+/*
+ * ParseNodeUID parses a UID of a node and returns the respective node IDs.
+ */
+func ParseNodeUID(id UID) HostID {
+	s := UIDSplit(id)
+	return HostID(s[0])
+}
+
+/*
+ * JSONDecode decodes the host from a byte array using JSON.
+ */
+func (n *Host) JSONDecode(b []byte) error {
+	return json.Unmarshal(b, n)
+}
+
+/*
+ * JSONEncode encodes the host into a byte array using JSON.
+ */
+func (n *Host) JSONEncode() ([]byte, error) {
+	return json.Marshal(n)
+}
+
+func init() {
+	gob.Register(HostLeft{})
+	gob.Register(HostJoined{})
+}
