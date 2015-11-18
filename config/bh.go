92913ba1650c992a77ca963e5b0116bee366e83c
Parham Alvani <parham.alvani@gmail.com>
2015-11-19 02:40:34 +0330

    Configuration module was improved.

--- config/bh.go
+++ config/bh.go
@@ -22,53 +22,52 @@ import (
 	"github.com/kandoo/beehive/Godeps/_workspace/src/golang.org/x/net/context"
 )
 
-type ConfigHTTPHandler struct{}
-
-func (h *ConfigHTTPHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	vars := mux.Vars(r)
-
-	app, ok := vars["app"]
-	if !ok {
-		http.Error(w, "Invalid request", http.StatusBadRequest)
-		return
-	}
-	fmt.Println(app)
-
-	feature, ok := vars["feature"]
-	if !ok {
-		http.Error(w, "Invalid request", http.StatusBadRequest)
-		return
-	}
-	fmt.Println(feature)
-
-	if r.Method == "POST" {
-		fmt.Println("Hello from POST message")
-	}
-
-	if r.Method == "GET" {
-		fmt.Println("Hello from GET message")
-	}
-
-	creq := ConfigRequest{
-		AppName:         app,
-		RequiredFeature: feature,
-	}
-
-	cres, err := bh.Sync(context.TODO(), creq)
-	if err == nil && cres != nil {
-		w.Header().Set("Server", "Beehive-netctrl-Config-Server")
-		w.Write(cres.(ConfigResponse).ResponseData)
-		return
-	} else {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-		fmt.Errorf("ConfigHTTPHandler: %v\n", err)
-		return
-	}
-}
-
 func StartConfig(h bh.Hive) error {
 	app := h.NewApp("ConfigApp", bh.Persistent(1))
-	app.HandleHTTP("/{app}/{feature}", &ConfigHTTPHandler{})
+
+	configHTTPHandler := func(w http.ResponseWriter, r *http.Request) {
+		vars := mux.Vars(r)
+
+		app, ok := vars["app"]
+		if !ok {
+			http.Error(w, "Invalid request", http.StatusBadRequest)
+			return
+		}
+		fmt.Println(app)
+
+		feature, ok := vars["feature"]
+		if !ok {
+			http.Error(w, "Invalid request", http.StatusBadRequest)
+			return
+		}
+		fmt.Println(feature)
+
+		if r.Method == "POST" {
+			fmt.Println("Hello from POST message")
+		}
+
+		if r.Method == "GET" {
+			fmt.Println("Hello from GET message")
+		}
+
+		creq := ConfigRequest{
+			AppName:         app,
+			RequiredFeature: feature,
+		}
+
+		cres, err := h.Sync(context.TODO(), creq)
+		if err == nil && cres != nil {
+			w.Header().Set("Server", "Beehive-netctrl-Config-Server")
+			w.Write(cres.(ConfigResponse).ResponseData)
+			return
+		} else {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			fmt.Errorf("ConfigHTTPHandler: %v\n", err)
+			return
+		}
+	}
+
+	app.HandleHTTPFunc("/{app}/{feature}", configHTTPHandler)
 
 	return nil
 }
