58da7d58d881c0600ff82b116523c8235066268d
Parham Alvani <parham.alvani@gmail.com>
2015-11-24 14:08:55 +0330

    NOM Host structure was improved.

--- nom/host.go
+++ nom/host.go
@@ -23,14 +23,15 @@ import (
 // TODO(elahe): add documentation ..
 type HostLeft Host
 
-/*
- * HostJoined is a message emitted when a host connects to network.
- */
+// HostJoined is a message emitted when a host connects to network and
+// we added it into our cells.
 type HostJoined Host
 
-/*
- * Host represnts a end point element, such as pc :)
- */
+// HostConnected is a message emitted when a host connects to network,
+// we use this event in order to add host into our cells.
+type HostConnected Host
+
+// Host represnts a end point element, such as your pc.
 type Host struct {
 	ID       HostID
 	Net      UID
@@ -38,48 +39,36 @@ type Host struct {
 	IPv4Addr IPv4Addr
 }
 
-/*
- * HostID is the ID of a host. This must be unique among all hosts in the
- * network
- */
+// HostID is the ID of a host. This must be unique among all hosts in
+// the network.
 type HostID string
 
 func (h Host) String() string {
 	return fmt.Sprintf("Host %s (mac=%v) (ip=%v)", string(h.ID), h.MACAddr, h.IPv4Addr)
 }
 
-/*
- * UID converts id into a UID.
- */
+// UID converts id into a UID.
 func (id HostID) UID() UID {
 	return UID(id)
 }
 
-/*
- * UID returns the node's unique ID. This id is in the form of net_id$$host_id.
- */
+// UID returns the node's unique ID. This id is in the form of net_id$$host_id.
 func (n Host) UID() UID {
 	return UID(string(n.ID))
 }
 
-/*
- * ParseHostUID parses a UID of a host and returns the respective host IDs.
- */
+// ParseHostUID parses a UID of a host and returns the respective host IDs.
 func ParseHostUID(id UID) HostID {
 	s := UIDSplit(id)
 	return HostID(s[0])
 }
 
-/*
- * JSONDecode decodes the host from a byte array using JSON.
- */
+// JSONDecode decodes the host from a byte array using JSON.
 func (n *Host) JSONDecode(b []byte) error {
 	return json.Unmarshal(b, n)
 }
 
-/*
- * JSONEncode encodes the host into a byte array using JSON.
- */
+// JSONEncode encodes the host into a byte array using JSON.
 func (n *Host) JSONEncode() ([]byte, error) {
 	return json.Marshal(n)
 }
