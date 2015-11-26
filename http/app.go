5b4d9c91b98200545b457e47ba8de55d0fc63947
Parham Alvani <parham.alvani@gmail.com>
2015-11-27 03:04:43 +0330

    Config module replaced with http module.

---
+++ http/app.go
@@ -0,0 +1,47 @@
+/*
+ * +===============================================
+ * | Author:        Elahe Jalalpour (el.jalalpour@gmail.com)
+ * |
+ * | Creation Date: 27-11-2015
+ * |
+ * | File Name:     app.go
+ * +===============================================
+ */
+package http
+
+import (
+	bh "github.com/kandoo/beehive"
+	"github.com/kandoo/beehive/Godeps/_workspace/src/github.com/gorilla/mux"
+	"net/http"
+)
+
+type HTTPHandlerFunc func(http.ResponseWriter, *http.Request, bh.Hive)
+type HTTPHandler interface {
+	Handler(http.ResponseWriter, *http.Request, bh.Hive)
+}
+
+type HTTPApp struct {
+	App  bh.App
+	Hive bh.Hive
+}
+
+func (h HTTPApp) HandleHTTPFunc(url string, handler HTTPHandlerFunc) *mux.Route {
+	std_handler := func(w http.ResponseWriter, r *http.Request) {
+		handler(w, r, h.Hive)
+	}
+	return h.App.HandleHTTPFunc(url, std_handler)
+}
+
+func (h HTTPApp) HandleHTTP(url string, handler HTTPHandler) *mux.Route {
+	std_handler := func(w http.ResponseWriter, r *http.Request) {
+		handler.Handler(w, r, h.Hive)
+	}
+	return h.App.HandleHTTPFunc(url, std_handler)
+}
+
+func NewHTTPApp(h bh.Hive, name string) HTTPApp {
+	return HTTPApp{
+		App:  bh.NewApp(name),
+		Hive: h,
+	}
+}
