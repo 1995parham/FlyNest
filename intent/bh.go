131b48ebdfa06ffd483f3d2dd0485f21e8327bbf
Parham Alvani <parham.alvani@gmail.com>
2016-01-22 23:34:32 +0330

    Minor issue was corrected.

--- intent/bh.go
+++ intent/bh.go
@@ -1,12 +1,11 @@
 package intent
 
 import (
+	"github.com/1995parham/flynest/discovery"
 	"github.com/1995parham/flynest/http"
 	"github.com/1995parham/flynest/nom"
 	bh "github.com/kandoo/beehive"
-	"github.com/kandoo/flynest/discovery"
 )
-
 
 func RegisterIntent(h bh.Hive) {
 	a := h.NewApp("intent")
