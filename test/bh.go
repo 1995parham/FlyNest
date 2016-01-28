c39463cac3d3bb36c5c2ea2f8636f0037d59b463
Parham Alvani <parham.alvani@gmail.com>
2016-01-28 10:35:53 +0330

    Test package was added for testing purpose.

---
+++ test/bh.go
@@ -0,0 +1,36 @@
+/*
+ * In The Name Of God
+ * ========================================
+ * [] File Name : bh.go
+ *
+ * [] Creation Date : 15-11-2015
+ *
+ * [] Created By : Parham Alvani (parham.alvani@gmail.com)
+ * =======================================
+ */
+/*
+ * Copyright (c) 2015 Parham Alvani.
+ */
+
+package test
+
+import (
+	"fmt"
+
+	nom "github.com/1995parham/flynest/nom"
+	bh "github.com/kandoo/beehive"
+)
+
+func hostJoinedRcvf(msg bh.Msg, ctx bh.RcvContext) error {
+	fmt.Println("Rcv of HostJoinedHandler Called")
+	fmt.Println(msg.Data().(nom.HostJoined))
+	return nil
+}
+
+func StartTest(hive bh.Hive) error {
+	app := hive.NewApp("TestApp")
+	fmt.Println("Test app is comming ... :)))")
+	app.HandleFunc(nom.HostJoined{}, bh.RuntimeMap(hostJoinedRcvf), hostJoinedRcvf)
+
+	return nil
+}
